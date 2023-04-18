d56ca736a59dc69f39a3ebb86e3a8df7b6ffb242
grace.fidele <gngrace10@gmail.com>
2023-04-18 21:58:36 +0200

    solve rebase conflicts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Require necessary modules
+<<<<<<< HEAD
 const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerUI = require("swagger-ui-express");
@@ -9,7 +10,24 @@ const schedulesRoute = require("./src/routes/protected/schedule");
 const usersRoute = require("./src/routes/protected/users");
 // Call config
 require("dotenv").config();
+=======
+import swaggerUI from "swagger-ui-express";
+//Routes
+import loginRoute from "./src/routes/open/login";
+import registerRoute from "./src/routes/open/signup";
+import schedualsRoute from "./src/routes/protected/schedual";
+import usersRoute from "./src/routes/protected/users";
+// Call config
+>>>>>>> 92e429f (merge with JMV's work on auth with JWT)
 // Create an instance of express app
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import passport from "passport";
+import googleAuth from "./routes/protected/admin.routes";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
 const app = express();
 //DB connectivity
 const connect = require("./src/models/DB");
@@ -34,9 +52,24 @@ app.set("view engine", "ejs");
 app.use("/v1/api/login", loginRoute);
 app.use("/v1/api/register", registerRoute);
 app.use("/v1/api/users", usersRoute);
+<<<<<<< HEAD
 app.use("/v1/api/schedules", schedulesRoute);
 app.use("/v1/api-docs", swaggerUI.serve, swaggerUI.setup(require("./swagger")));
 
+=======
+app.use("/v1/api/scheduals", schedualsRoute);
+app.use("/v1/api-docs", swaggerUI.serve, swaggerUI.setup(require("./swagger")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", googleAuth);
+
+app.get("/", (req, res) => {
+  console.log(`Starting...`);
+});
+
+>>>>>>> 92e429f (merge with JMV's work on auth with JWT)
 app.listen(process.env.PORT, () =>
   console.log(`server is running on http://localhost:${process.env.PORT}`)
 );
